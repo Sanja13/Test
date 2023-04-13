6ebb552ae7fbdbc7576435720a4939161681d580
Sanja13 <130696114+Sanja13@users.noreply.github.com>
2023-04-13 21:18:53 +0200

    Fixed error for int number and added 2 to the number

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int number = Console.ReadLine();
+
+Console.WriteLine("Vnesete broj od tastatura");
+int number = int.Parse(Console.ReadLine());
+number += 2;
+Console.WriteLine("Rezultat" + number.ToString());
 Console.WriteLine("Hello, Angel!!!");
