a615ab8cda9c3d1d2a20268d8a421d928c808cf2
Sanja13 <130696114+Sanja13@users.noreply.github.com>
2023-04-13 21:50:28 +0200

    programa koja presmetuva zbir na prvite 10 broevi

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -1,7 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Vnesete broj od tastatura");
-int number = int.Parse(Console.ReadLine());
-number += 2;
-Console.WriteLine($"Rezultat {number}");
-Console.WriteLine("Hello, Angel!!!");
+//Console.WriteLine("Vnesete broj od tastatura");
+//int number = int.Parse(Console.ReadLine());
+//number += 2;
+//Console.WriteLine($"Rezultat {number}");
+//Console.WriteLine("Hello, Angel!!!");
+
+int zbir = 0;
+for(int i=1; i <= 10; i++)
+{
+    zbir += i;
+}
+Console.WriteLine(zbir);
